0939a179f21ccc0b95555a499ab676dce81725b4
danie <danie@DESKTOP-2JINL1V>
2022-07-03 14:03:48 -0300

    Adicionar arquivos de projeto.

---
+++ Segundo Exercicio Prova do Djalma/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segundo_Exercicio_Prova_do_Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            string cidade = null;
+            Console.WriteLine(" 1–Atibaia" + "\n" + "2–Bragança Paulista" + "\n" + "3-Mairiporã" + "\n" + "4-Nazaré" + "\n" + "5-Terra Preta" + "\n" + "6-Extrema" + "\n" + "7-Vargem");
+            Console.WriteLine("Informe o codigo da cidade escolhida.");
+            codigo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("O codigo é: " + codigo);
+            switch (codigo)
+            {
+                case 1:
+                    cidade = "Atibaia";
+                    break;
+
+                case 2:
+                    cidade = "Bragança Paulista";
+                    break;
+
+                case 3:
+                    cidade = "Mairiporã";
+                    break;
+
+                case 4:
+                    cidade = "Nazaré";
+                    break;
+
+                case 5:
+                    cidade = "Terra Preta";
+                    break;
+
+                case 6:
+                    cidade = "Extrema";
+                    break;
+
+                case 7:
+                    cidade = "Vargem";
+                    break;
+
+
+
+
+
+            }
+            int mes, dia, ano;
+            string mesextenso = null;
+            Console.WriteLine("Digite uma Data ");
+            var dataatual = Convert.ToDateTime(Console.ReadLine());
+            mes = dataatual.Month;
+            dia = dataatual.Day;
+            ano = dataatual.Year;
+            switch (mes)
+
+            {
+                case 1:
+                    mesextenso = "Janeiro";
+                    break;
+
+                case 2:
+                    mesextenso = "Fevereiro";
+                    break;
+
+                case 3:
+                    mesextenso = "Março";
+                    break;
+
+                case 4:
+                    mesextenso = "Abril";
+                    break;
+
+                case 5:
+                    mesextenso = "Maio";
+                    break;
+
+                case 6:
+                    mesextenso = "Junho";
+                    break;
+
+                case 7:
+                    mesextenso = "Julho";
+                    break;
+
+                case 8:
+                    mesextenso = "Agosto";
+                    break;
+
+                case 9:
+                    mesextenso = "Setembro";
+                    break;
+
+                case 10:
+                    mesextenso = "Otubro";
+                    break;
+
+                case 11:
+                    mesextenso = "Novembro";
+                    break;
+
+                case 12:
+                    mesextenso = "Dezembro";
+                    break;
+
+
+            }
+
+
+
+            ExibirMensagem(cidade+","+dia+"de"+mesextenso+"de"+ano);
+
+            Console.ReadKey();
+
+        }
+        static void ExibirMensagem(string texto)
+        {
+            
+            Console.WriteLine(texto);
+
+        }
+
+
+    }
+
+
+            
+           
+
+        
+          
+    
+}
+
+
+
+
+
+
+
